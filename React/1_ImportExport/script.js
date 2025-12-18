e1b957ab74280b08c786e61c2c38e6e9d0916166
Ajay Gour <theajaygour@gmail.com>
2025-12-18 21:59:33 +0530

    day1

---
+++ React/1_ImportExport/script.js
@@ -0,0 +1,17 @@
+
+
+// const user = {
+//     name:'ajay',
+//     age:19
+// };
+
+
+// // normally by the default parameter
+// export default user;
+
+
+
+// now by name parameter
+
+export let age = 19;
+export let name = "Ajay";
