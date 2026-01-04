f78accacb76c90b44d2fb96c9f11d4d6edb7b18f
Ajay Gour <theajaygour@gmail.com>
2026-01-04 12:09:11 +0530

    10

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+
+import Upper from './Upper'
+import Lower from './Lower'
+
+const Card = (props) => {
+
+
+
+    return (
+        <div className='card'>
+            <Upper cardData={props.cardData} />
+            <Lower cardData={props.cardData} />
+        </div>
+    )
+}
+
+export default Card
